859e569beea23fec4f23604b4915a089731a0e8d
Bakerling25 <thom97m6@edu.sde.dk>
2020-01-15 14:12:06 +0100

    Mangler Bare Det Sidste

---
+++ Opgave6/MethodExcercise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opgave6
+{
+    class MethodExcercise
+    {
+        public int[] RotateRight(int[] inputArr,int numberOfSteps)
+        {
+            int rotateNum = numberOfSteps;
+            int[] outputArr = new int[inputArr.Length];
+            for (int i = 0; i < inputArr.Length -1; i++)
+            {
+                if (i + (inputArr.Length -1) <= inputArr.Length)
+                {
+                    outputArr[i + numberOfSteps] = inputArr[i];
+                }
+                else
+                {
+                    outputArr[]
+                }
+            }
+            return outputArr;
+        }
+    }
+}
---
+++ Opgave6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Opgave6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = new int[] { 6, 3, 8, 9, 7 };
+            int numberOfRightSteps = 3;
+            MethodExcercise method = new MethodExcercise();
+            foreach (int num in method.RotateRight(inputArray,numberOfRightSteps))
+            {
+                Console.Write(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
